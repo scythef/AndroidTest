2a88058ec80cb1552d5b580b281f482cfb4878ff
Filip Řehořík <filip.rehorik@hotmail.com>
2017-12-05 10:59:11 +0100

    Add project files.

---
+++ Android1/Android1.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Android1.Droid
+{
+	[Activity (Label = "Android1", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new Android1.App ());
+		}
+	}
+}
+
